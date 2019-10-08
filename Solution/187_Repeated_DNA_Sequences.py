89e7da5585abe63556bd2b684e3facfebeb60988
raririn <raririn.sandbag@gmail.com>
2019-10-07 21:09:34 -0400

    add 187

---
+++ Solution/187_Repeated_DNA_Sequences.py
@@ -0,0 +1,25 @@
+class Solution:
+    def findRepeatedDnaSequences(self, s: str) -> List[str]:
+        if len(s) <= 10:
+            return []
+        def twoWayHash(text):
+            ret = 0
+            for i in text:
+                ret = ret * 27 + (ord(i)-64)
+            return ret
+
+        def toStr(hash):
+            t = u""
+            while hash > 0:
+                t = chr(hash % 27+64) + t
+                hash = hash // 27
+            return t
+
+        d = {}
+        ret = []
+        for i in range(len(s)-9):
+            h = twoWayHash(s[i:i+10])
+            if d.get(h, 0) == 1:
+                ret.append(toStr(h))
+            d[h] = d.get(h, 0) + 1
+        return ret
