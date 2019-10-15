cd8e503f8fd08f507423d241119e202cc194a4c0
raririn <raririn.sandbag@gmail.com>
2019-10-15 23:04:02 +0000

    sync from ipad

--- Solution/215_Kth_Largest_Element_in_an_Array.py
+++ Solution/215_Kth_Largest_Element_in_an_Array.py
@@ -1,3 +1,17 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        
+        pos = 0
+        while pos != k:
+            median = nums[len(nums)//2]
+            pos = 0
+            for i in nums:
+                if i <= median:
+                    pos = pos + 1
+            nums[pos], nums[median] = nums[median], nums[pos]
+            if pos > k:
+                num = num[:pos]
+            elif pos < k:
+                num = [pos:]
+                k = k - pos
+            else:
+                return num[pos]
